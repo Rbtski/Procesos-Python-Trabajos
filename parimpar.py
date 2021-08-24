e13fca9d385bb214795df5587441ce8100ab3a36
Rbtski <Robertoski_da@hotmail.com>
2021-08-23 23:11:38 -0400

    Par&Impar

--- parimpar.py
+++ parimpar.py
@@ -1,13 +1,13 @@
 n = int(input("Ingrese Un Numero Entero: "))
 
-
+#Operaciones
 if n!= 0:
-    if n > 0:
+    if n > 0: #mayor a 0
        if n % 2 == 0: #True
          print(f"El Numero {n} es Par Positivo")
        else:    
          print(f"El Numero {n} es Impar Positivo")
-    else:
+    else: #si es menor a 0
        if n % 2 == 0: #True
           print(f"El Numero {n} es Par Negativo")
        else:    
@@ -15,4 +15,4 @@ if n!= 0:
 else:
      print(f"El Numero {n} es Neutro")
 
-#Operaciones
+
