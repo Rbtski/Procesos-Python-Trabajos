d78b9cb1bbb0efeee8115b3ad14687726c617282
Rbtski <Robertoski_da@hotmail.com>
2021-08-26 14:12:06 -0400

    elif

---
+++ elif.py
@@ -0,0 +1,14 @@
+v = input("Ingrese un Caracter: ")
+#puede colocar tantas elif como sea necesario entre if y else.
+if v == "a" or v == "A":
+    print(v, "Es Vocal")
+elif v == "e" or v == "E":
+    print(v, "Es Vocal")
+elif v == "i" or v == "I":
+    print(v, "Es Vocal")
+elif v == "o" or v == "O":
+    print(v, "Es Vocal")
+elif v == "u" or v == "U":
+    print(v, "Es Vocal")
+else:
+    print(v, "No es Vocal")
